ec0ce4f35351e37e0afb919740aa3abf835ebc37
Kirill <kirilldevelop1511@mail.ru>
2025-06-14 15:49:12 +0300

    A 'discoery' page was created and added loading data from server

---
+++ discovery.js
@@ -0,0 +1,107 @@
+// function makeRequest(url) {
+//   return new Promise((fullfill, reject) => {
+//     fetch(url)
+//     .then(responce => {
+//       if (!responce.ok) {
+//         throw new Error("Error: " + responce.status);
+//       }
+//       return  responce.json();
+//     })
+//     .then(data => {
+//       fullfill(data);
+//     })
+//     .catch(error => {
+//       reject(error);
+//     })
+//   })
+// }
+
+// function addCollectionCards(data) {
+//   const collectionList = document.querySelector(".collection_list");
+//   for (let i = 0; i < data.length; i++) {
+//     const card = `
+//       <li class="card">
+//           <div class="img" style="background-image: url('${data[i].nftImage}');">
+//               <p>07h 09m 12s</p>
+//           </div>
+//           <h1>${data[i].title}</h1>
+//           <div class="prices">
+//               <div class="prices_info">
+//                   <h4>Current bid</h4>
+//                   <p><img src="Resource/etherium.png"> ${data[i].price}</p>
+//               </div>
+//               <button>Place Bid</button>
+//           </div>
+//       </li>
+//     `;
+    
+//     collectionList.insertAdjacentHTML('beforeend', card);
+//   }
+// }
+
+
+// makeRequest("https://6834a37ecd78db2058bee2cc.mockapi.io/collection")
+//   .then((data) => {
+//     addCollectionCards(data);
+//   })
+//   .catch((error) => {
+//     console.log("ERROR: " + error);
+//   })
+
+
+
+// MARK: Переделанный код под async/await
+
+async function makeRequest(url) {
+  return new Promise((fullfill, reject) => {
+    fetch(url)
+    .then(responce => {
+      if (!responce.ok) {
+        throw new Error("Error: " + responce.status);
+      }
+      return  responce.json();
+    })
+    .then(data => {
+      fullfill(data);
+    })
+    .catch(error => {
+      reject(error);
+    })
+  })
+}
+
+function addCollectionCards(data) {
+  const collectionList = document.querySelector(".collection_list");
+  for (let i = 0; i < data.length; i++) {
+    const card = `
+      <li class="card">
+          <div class="img" style="background-image: url('${data[i].nftImage}');">
+              <p>07h 09m 12s</p>
+          </div>
+          <h1>${data[i].title}</h1>
+          <div class="prices">
+              <div class="prices_info">
+                  <h4>Current bid</h4>
+                  <p><img src="Resource/etherium.png"> ${data[i].price}</p>
+              </div>
+              <button>Place Bid</button>
+          </div>
+      </li>
+    `;
+    
+    collectionList.insertAdjacentHTML('beforeend', card);
+  }
+}
+
+
+async function drawCards() {
+  try {
+    const data = await makeRequest("https://6834a37ecd78db2058bee2cc.mockapi.io/collection");
+    addCollectionCards(data);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+drawCards();
+
--- home.js
+++ home.js
@@ -13,31 +13,133 @@ for(let i = 0; i < asks.length; i++) {
     }
   })
 }
+
 // MARK: SLIDER
-const cardSlider = document.querySelector(".cards_slider ul")
+function SliderInit() {
+  const cardSlider = document.querySelector(".cards_slider ul")
 
-const prevBtn = document.querySelector(".prev_item");
-const nextBtn = document.querySelector(".next_item");
-let position = 0;
-const maxPosition = Math.floor(cardSlider.querySelectorAll("li").length / 2) * 340
-const minPosition = Math.floor(cardSlider.querySelectorAll("li").length / 2) * -340
+  const prevBtn = document.querySelector(".prev_item");
+  const nextBtn = document.querySelector(".next_item");
+  let position = 0;
+  const maxPosition = Math.floor(cardSlider.querySelectorAll("li").length / 2) * 340
+  const minPosition = Math.floor(cardSlider.querySelectorAll("li").length / 2) * -340
 
-prevBtn.addEventListener('click', function() {
-  if(position >= maxPosition) {
-    position = minPosition;
-  } else {
-    position = position + 340;
-  }
+  console.log(cardSlider.querySelectorAll("li").length)
 
-  cardSlider.style.left = position + "px";
-})
+  prevBtn.addEventListener('click', function() {
+    if(position >= maxPosition) {
+      position = minPosition;
+    } else {
+      position = position + 340;
+    }
 
-nextBtn.addEventListener('click', function() {
-  if(position <= minPosition) {
-    position = maxPosition;
-  } else {
-    position = position - 340;
-  }
+    cardSlider.style.left = position + "px";
+  })
 
-  cardSlider.style.left = position + "px";
-})
+  nextBtn.addEventListener('click', function() {
+    if(position <= minPosition) {
+      position = maxPosition;
+    } else {
+      position = position - 340;
+    }
+
+    cardSlider.style.left = position + "px";
+  })
+
+}
+
+
+
+
+
+
+// MARK: WeeklyTop
+
+function getTopWeek() {
+  return new Promise((resolve, reject) => {
+   fetch("https://6834a37ecd78db2058bee2cc.mockapi.io/weekly_top")
+  .then(responce => {
+    if (!responce.ok) {
+      throw new Error("Error from  get data: " + responce.status)
+    }
+    resolve("success");
+    return responce.json();
+  })
+  .then((data) => {
+    const weeklyTop = document.querySelector(".cards_slider ul")
+    for (let i = 0; i < data.length; i++) {
+      const htmlCard = `
+        <li class="card">
+            <div class="img" style="background-image: url('${data[i].avatar}');">
+                <p>07h 09m 12s</p>
+            </div>
+            <h1>${data[i].name}</h1>
+            <div class="prices">
+                <div class="prices_info">
+                    <h4>Current bid</h4>
+                    <p><img src="Resource/etherium.png"> ${data[i].price}</p>
+                </div>
+                <button>Place Bid</button>
+            </div>
+        </li>
+      `
+
+      weeklyTop.insertAdjacentHTML('beforeend', htmlCard)
+    }
+
+    SliderInit();
+  })
+  .catch(error => {
+    console.log("Error: " + error)
+  })
+  })
+}
+
+
+getTopWeek();
+
+
+// MARK: Collections
+
+const marketplaceCards = document.querySelector(".exp_market .products_cards ul")
+
+function getMarketCollection() {
+  return new Promise((resolve, reject) => {
+    fetch("https://6834a37ecd78db2058bee2cc.mockapi.io/collection")
+    .then(responce => {
+      if(!responce.ok) {
+        throw new Error("Error: " + responce.status)
+      }
+      resolve("success");
+      return responce.json()
+    })
+    .then((data) => {
+      for (let i = 0; i < 8; i++) {
+        const card = `
+          <li class="card">
+              <div class="img" style="background-image: url('${data[i].nftImage}');">
+                  <p>07h 09m 12s</p>
+              </div>
+              <h1>${data[i].title}</h1>
+              <div class="prices">
+                  <div class="prices_info">
+                      <h4>Current bid</h4>
+                      <p><img src="Resource/etherium.png"> ${data[i].price}</p>
+                  </div>
+                  <button>Place Bid</button>
+              </div>
+          </li>
+        `
+        console.log(data[i]);
+        
+        marketplaceCards.insertAdjacentHTML('beforeend', card)
+      }
+    })
+    .catch(error => {
+      console.log("ERROR: " + error)
+    })
+  })
+  
+}
+
+getMarketCollection();
